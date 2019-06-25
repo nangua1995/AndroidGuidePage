1b3ce799d0c5a27a35c1149fa97ae2767fd936a1
baihe <wanghs0315@thundersoft.com>
2019-06-25 14:58:26 +0800

    Initial View Pager Code

---
+++ app/src/main/java/com/example/viewpagertest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.viewpagertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private ArrayList<View> aList;
+    private ViewPagerAdapter adpter;
+
+    private int vp_viewer[] ={R.layout.guid_view1,R.layout.guid_view2,R.layout.guid_view3,
+                             R.layout.guid_view4};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.vp);
+
+        aList = new ArrayList<View>();
+
+        for(int i=0;i<vp_viewer.length;i++){
+            View view = LayoutInflater.from(this).inflate(vp_viewer[i],null);
+            aList.add(view);
+        }
+
+        adpter = new ViewPagerAdapter(aList);
+        viewPager.setAdapter(adpter);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/viewpagertest/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.viewpagertest;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    private ArrayList<View> viewLists;
+
+    public ViewPagerAdapter(){
+
+    }
+    public ViewPagerAdapter(ArrayList<View> viewLists){
+        super();
+        this.viewLists = viewLists;
+    }
+
+    @Override
+    public int getCount() {
+        return viewLists.size();
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(viewLists.get(position));
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ((ViewPager)container).addView(viewLists.get(position));
+        return viewLists.get(position);
+    }
+}
